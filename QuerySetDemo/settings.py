ae0273e3a18eb08fe735d3f165aab5eaafab335d
diegohwang <diegohwang@163.com>
2016-05-13 18:22:31 +0800

    commit model

--- QuerySetDemo/settings.py
+++ QuerySetDemo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'people',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'QuerySetDemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'queryset',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST':'192.168.16.164',
+        'PORT': '3306',
     }
 }
 
--- people/models.py
+++ people/models.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals#必须放在首行
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
+
+
+class man(models.Model):
+    name = models.CharField('姓名', max_length=128)
+    age = models.CharField('年龄', max_length=128)
+    email = models.TextField('邮箱', default='')
+        
+    def __str__(self):
+        return self.name
+    
+class subject(models.Model):
+    
